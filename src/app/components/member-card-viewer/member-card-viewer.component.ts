66577ba693be7c62439204a69dbee6bfbeace240
selestino <selestinoolivier@gmail.com>
2025-11-07 09:29:40 +0300

    @selestinohajaniaina add stamp and signature to badge

--- src/app/components/member-card-viewer/member-card-viewer.component.ts
+++ src/app/components/member-card-viewer/member-card-viewer.component.ts
@@ -1,17 +1,19 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Member } from '../../interfaces/member';
 import { MemberBadgeComponent } from '../member-badge/member-badge.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-member-card-viewer',
   standalone: true,
-  imports: [MemberBadgeComponent],
+  imports: [MemberBadgeComponent, FormsModule],
   templateUrl: './member-card-viewer.component.html',
   styleUrl: './member-card-viewer.component.css'
 })
 export class MemberCardViewerComponent {
   @Input() member!: Member[];
   @Output('closed') emiterClose = new EventEmitter<boolean>();
+  public checkData: { stamp: boolean, signature: boolean } = { stamp: false, signature: false }
   
   close() {
     this.emiterClose.emit(true);
